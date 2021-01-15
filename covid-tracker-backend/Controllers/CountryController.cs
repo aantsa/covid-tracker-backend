dae5dbcbe91cc0e206d84ebead372ee4813546e5
aantsa <anton.fagerholm@gmail.com>
2021-01-15 13:29:44 +0200

    initial commit

---
+++ covid-tracker-backend/Controllers/CountryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace star_wars.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountryController : ControllerBase
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+
+        [HttpGet]
+        public async Task<string> GetCountries(string uri)
+        {
+            try
+            {
+                var baseUrl = "https://covid-api.mmediagroup.fr/v1/cases/";
+                HttpResponseMessage response = await client.GetAsync(baseUrl);
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException ex)
+            {
+                return ex.Message;
+            }
+        }
+
+    }
+}
---
+++ covid-tracker-backend/Models/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace covid_tracker_backend.Models
+{
+    public class Country
+    {
+        public int Confirmed { get; set; }
+        public int Recovered { get; set; }
+        public int Deaths { get; set; }
+        public string CountryName { get; set; }
+        public int Population { get; set; }
+        public int SqKmArea { get; set; }
+        public string LifeExpectancy { get; set; }
+        public string Continent { get; set; }
+        public string Location { get; set; }
+        public string Capital { get; set; }
+    }
+}
